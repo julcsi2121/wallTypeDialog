9525d96b2ae3107c9d79ea923930e71016374fa4
julcsi2121 <julcsi2121@gmail.com>
2019-10-22 14:09:59 +0200

    dialog toggle logic included

--- src/App.js
+++ src/App.js
@@ -1,10 +1,38 @@
 import React from 'react';
 //import './App.css';
 
+class WallDialog extends React.Component{
+  render() {
+    return(
+        <div>
+          <h1>Wall type dialog opened</h1>
+          <button onClick={this.props.toggleDialog}>Close dialog</button>
+        </div>
+    )
+  }
+}
+
 class App extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state = {isDialogOpen: false};
+
+    this.toggleDialog = this.toggleDialog.bind(this);
+  }
+
+  toggleDialog(){
+    this.setState({isDialogOpen: !this.state.isDialogOpen});
+    //console.log(this.state);
+  }
+
   render() {
     return (
-        <div>Initial content</div>
+        <div>
+          <button onClick={this.toggleDialog}>Open wall-type dialog</button>
+          {this.state.isDialogOpen ?
+          <WallDialog toggleDialog={this.toggleDialog.bind(this)}/>
+          : null}
+        </div>
     )
   }
 }
