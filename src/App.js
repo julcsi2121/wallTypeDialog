27f75a85842edbf964e7ac96c1227aff6ed3923a
julcsi2121 <julcsi2121@gmail.com>
2019-10-22 19:45:10 +0200

    logic about rendering list of objects included

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
 import React from 'react';
-//import './App.css';
+import './App.css';
 
 class WallDialog extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {textToShow: ""};
+  }
+
+  cica = {species: "cica", text: "A cicák nagyon aranyos állatok."};
+  kutya = {species: "kutya", text: "A kutyák nagyon okos állatok."};
+  delfin = {species: "delfin", text: "A delfinek nagyon szép állatok."};
+
+  data = [this.cica, this.kutya, this.delfin];
+
+  selectAnimal(element){
+    this.setState({textToShow: element.text});
+    console.log(element.species);
+  }
+
+  listElement = this.data.map((element) => <li key={element.species}><button onClick={() => this.selectAnimal(element)}>{element.species}</button></li>);
+
   render() {
     return(
         <div>
           <h1>Wall type dialog opened</h1>
-          <button onClick={this.props.toggleDialog}>Close dialog</button>
+          <ul>{this.listElement}</ul>
+          <p>{this.state.textToShow}</p>
+          <button onClick={this.props.closeDialog}>Close dialog</button>
         </div>
     )
   }
@@ -17,20 +37,25 @@ class App extends React.Component{
     super(props);
     this.state = {isDialogOpen: false};
 
-    this.toggleDialog = this.toggleDialog.bind(this);
+    this.openDialog = this.openDialog.bind(this);
   }
 
-  toggleDialog(){
-    this.setState({isDialogOpen: !this.state.isDialogOpen});
+  openDialog(){
+    this.setState({isDialogOpen: true});
+    //console.log(this.state);
+  }
+
+  closeDialog(){
+    this.setState({isDialogOpen: false});
     //console.log(this.state);
   }
 
   render() {
     return (
         <div>
-          <button onClick={this.toggleDialog}>Open wall-type dialog</button>
+          <button onClick={this.openDialog}>Open wall-type dialog</button>
           {this.state.isDialogOpen ?
-          <WallDialog toggleDialog={this.toggleDialog.bind(this)}/>
+          <WallDialog closeDialog={this.closeDialog.bind(this)}/>
           : null}
         </div>
     )
